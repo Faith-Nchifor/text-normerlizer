3469afd693135936f5d697ef9fd4ba02eb48bdda
Faith Nchifor <faithnchifor@gmail.com>
2025-11-23 21:50:27 +0100

    removed double imports from normalize_fr.py

--- src/normalize_fr.py
+++ src/normalize_fr.py
@@ -1,7 +1,7 @@
 '''
 Create FSTs for numbers from 0 - 1000 in French
 '''
-
+import os
 import pynini
 from utils import I_O_FST, far_dir, apply_fst
 
@@ -40,44 +40,6 @@ hundreds = {
     "900": "neuf cents"
 }
 
-import os
-import pynini
-from utils import I_O_FST, far_dir # will use later
-
-units_map = {
-    "0": "zero", "1": "one", "2": "two", "3": "three", "4": "four",
-    "5": "five", "6": "six", "7": "seven", "8": "eight", "9": "nine"
-}
-
-
-teens_map = {
-    "10": "ten", "11": "eleven", "12": "twelve", "13": "thirteen",
-    "14": "fourteen", "15": "fifteen", "16": "sixteen",
-    "17": "seventeen", "18": "eighteen", "19": "nineteen"
-}
-
-tens_digit_map = {
-    "2": "twenty",
-    "3": "thirty",
-    "4": "forty",
-    "5": "fifty",
-    "6": "sixty",
-    "7": "seventy",
-    "8": "eighty",
-    "9": "ninety"
-}
-
-hundreds = {
-    "100": "one hundred",
-    "200": "two hundred",
-    "300": "three hundred",
-    "400": "four hundred",
-    "500": "five hundred",
-    "600": "six hundred",
-    "700": "seven hundred",
-    "800": "eight hundred",
-    "900": "nine hundred"
-}
 
 # FST to insert a space: outputs " "
 fst_insert_space = I_O_FST("", " ") # <eps> -> " "
